db824fa6d88a464539ab32fe9c357a3b115041ea
admin <admin@TeamCity>
2025-03-17 14:23:45 +0530

    TeamCity change in 'Simple React App' project: 2 build step(s) were added to 'Build' build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,10 +1,35 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.buildSteps.nodeJS
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
 
 version = "2024.12"
 
 project {
+
     buildType(Build)
 }
 
@@ -16,22 +41,25 @@ object Build : BuildType({
     }
 
     steps {
-        script {
-            name = "Installing npm packages"
-            scriptContent = "npm install"
+        maven {
+            id = "Maven2"
+            goals = "clean test"
+            pomLocation = ".teamcity/pom.xml"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
-
-        script {
-            name = "Run tests"
-            scriptContent = "npm run verify"
+        nodeJS {
+            id = "nodejs_runner"
+            shellScript = "npm run test"
         }
     }
 
     triggers {
-        vcs { }
+        vcs {
+        }
     }
 
     features {
-        perfmon { }
+        perfmon {
+        }
     }
 })
